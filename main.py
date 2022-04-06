48b5af0784824b9312ed66cfb5c9c09cc389101c
ewave99 <ehmurdoch47@protonmail.com>
2022-04-06 21:37:09 +0100

    moved second version to be first version

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 
 class App:
-
+    
     def __init__(self):
         self.interpreter = Interpreter()
 
     def run(self):
         while True:
             user_input = input()
-            self.interpreter.run(user_input)
+            self.interpreter.processInput(user_input)
 
 
 class Interpreter:
@@ -16,114 +16,193 @@ class Interpreter:
     def __init__(self):
         self.machine = Machine()
 
-    def run(self, user_input):
-        words = user_input.split(' ')
-        for word in words:
-            self.machine.execute(word)
+    def processInput(self, user_input):
+        tokens = user_input.split(' ')
+        try:
+            for token in tokens:
+                self.machine.processToken(token)
+        except Exception as e:
+            print(e)
 
 
 class Machine:
 
     def __init__(self):
-        self.block_level = 0
-        self.stack = Block(self.block_level)
-        self.hierarchy_stack = Block(0)
-        self.current_defs = self.hierarchy_stack.defs
+        self.stack = Stack()
+        self.scope_stack = Stack()
 
-    def execute(self, word):
-        if word == "{":
-            self.block_level += 1
-            block = Block(self.block_level)
-            block.defs.update(self.current_defs)
-            self.stack.push(block)
-            self.hierarchy_stack.push(block)
+        self.word_builder = WordBuilder()
 
-        elif word == "}":
-            b = self.stack.pop()
-            # if block b is not closed
-            if self.block_level >= b.level:
-                # close it
-                self.block_level -= 1
+    def processToken(self, token):
+        item = self.getItem(token)
+        item.execute(self)
+
+    def getItem(self, token):
+        is_int = True
+        try:
+            item = Literal(int, token)
+        except ValueError:
+            is_int = False
+        if is_int:
+            return item
+
+        is_float = True
+        try:
+            item = Literal(float, token)
+        except ValueError:
+            is_float = False
+        if is_float:
+            return item
+
+        is_word = True
+        try:
+            item = self.word_builder.createWord(self, token)
+        except KeyError:
+            is_word = False
+        if is_word:
+            return item
+
+    def getDefinition(self, token):
+        scope = self.getScope()
+        if token in scope:
+            return scope[token]
+        raise Exception("Unknown token: {}".format(token))
+
+    def getScope(self):
+        if not self.scope_stack:
+            return dict()
+        return self.scope_stack.peek()
 
 
-            # if we have already closed the last block
-            # then we are closing the parent
-            else:
-                # Revert the defs to the parent.
-                # If the block does not have a parent:
-                if b.level <= 1:
-                    self.stack.push(b)
-                    self.current_defs = self.stack.defs
-                else:
-                    # we are using a block as a temporary stack here
-                    children_stack = Block(0)
-                    children_stack.push(b)
-                    child = self.stack.pop()
-                    while not (isinstance(child, Block) and child.level < self.block_level):
-                        children_stack.push(child)
-                        child = self.stack.pop()
-                    parent = child
-                    while children_stack:
-                        child = children_stack.pop()
-                        parent.push(child)
-                    self.stack.push(parent)
-                    self.current_defs = parent.defs
+class Stack:
 
-        elif word == ".s":
-            print(self.stack)
+    def __init__(self):
+        self._list = list()
 
-        else:
-            if self.block_level <= 0:
-                if word == "define":
-                    name_block = self.stack.pop()
-                    if not isinstance(name_block, block):
-                        raise exception("definition name should be block with length 1.")
-                    if len(block) != 1:
-                        raise exception("invalid name for definition.")
-                    code_block = self.stack.pop()
-                    if not isinstance(code_block, block):
-                        raise exception("definition body must be block.")
-                    name = name_block.items[0]
-                    self.stack.defs[name] = code_block
+    def __len__(self):
+        return len(self._list)
+    
+    def __iter__(self):
+        yield from self._list
 
-                elif word in self.stack.defs:
-                    self.executeBlock(self.stack.defs[word])
-            else:
-                top = self.stack.pop()
-                top.push(word)
-                self.stack.push(top)
+    def push(self, item):
+        self._list.append(item)
 
-    def executeBlock(self, block):
-        pass
+    def pop(self):
+        return self._list.pop()
+
+    def peek(self):
+        return self._list[-1]
+
+
+class WordBuilder:
+    def __init__(self):
+        self.builtins = dict()
+
+        self.builtins["+"] = self.createTwoValFunction(lambda a, b: a + b)
+        self.builtins["-"] = self.createTwoValFunction(lambda a, b: a - b)
+        self.builtins["*"] = self.createTwoValFunction(lambda a, b: a * b)
+        self.builtins["/"] = self.createTwoValFunction(lambda a, b: a / b)
+        self.builtins["%"] = self.createTwoValFunction(lambda a, b: a % b)
+        self.builtins["//"] = self.createTwoValFunction(lambda a, b: a // b)
+        self.builtins["**"] = self.createTwoValFunction(lambda a, b: a ** b)
+
+        self.builtins[".s"] = self.printStack
+        self.builtins["."] = self.printTop
+
+        self.builtins["drop"] = self.drop
+        self.builtins["dup"] = self.dup
+        self.builtins["swap"] = self.swap
+
+    def createWord(self, machine, token):
+        func = self.getWordFunction(machine, token)
+        word = Word(func)
+        return word
+
+    def getWordFunction(self, machine, token):
+        if token in self.builtins:
+            return self.builtins[token]
+        return machine.getDefinition(token)
+
+    def createOneValFunction(self, base_func):
+
+        def func(machine):
+            item = machine.stack.pop()
+            result = base_func(item)
+            machine.stack.push(result)
+
+        return func
+
+    def createTwoValFunction(self, base_func):
+
+        def func(machine):
+            b = machine.stack.pop()
+            a = machine.stack.pop()
+            result = base_func(a, b)
+            machine.stack.push(result)
+
+        return func
+
+    def printStack(self, machine):
+        string = ""
+        for item in machine.stack:
+            string += "{} ".format(item)
+        string += "<- TOP"
+        print(string)
+
+    def printTop(self, machine):
+        item = machine.stack.pop()
+        print(item)
+
+    def drop(self, machine):
+        machine.stack.pop()
+
+    def dup(self, machine):
+        item = machine.stack.pop()
+        machine.stack.push(item)
+        machine.stack.push(item)
+
+    def swap(self, machine):
+        b = machine.stack.pop()
+        a = machine.stack.pop()
+        machine.stack.push(b)
+        machine.stack.push(a)
+
+
+class Word:
+
+    def __init__(self, func):
+        self.func = func
+
+    def execute(self, machine):
+        self.func(machine)
 
 
 class Block:
 
-    def __init__(self, level):
-        self.level = level
-        self.items = list()
-        self.defs = dict()
-        self.closed = False
+    def __init__(self, children):
+        pass
 
-    def __len__(self):
-        return len(self.items)
+    def execute(self, machine):
+        pass
 
-    def __iter__(self):
-        yield from self.items
 
-    def __repr__(self):
-        string = "{ "
-        for item in self.items:
-            string += "{} ".format(repr(item))
-        string += "}"
-        string += ":{}".format(self.level)
-        return string
+class Literal:
 
-    def push(self, item):
-        self.items.append(item)
+    def __init__(self, baseclass, identifier):
+        self.value = baseclass(identifier)
 
-    def pop(self):
-        return self.items.pop()
+    def execute(self, machine):
+        machine.stack.push(self.value)
+
+
+class Definition:
+
+    def __init__(self, identifier, sequence):
+        pass
+
+    def execute(self, machine):
+        pass
 
 if __name__ == "__main__":
     app = App()
